54b3e68b45ea05492b51ce12120f644a2c0b733e
Matthieu Drula <matthieu.drula.hs@gmail.com>
2018-07-10 18:15:09 +0200

    tuto working

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<!-- ./src/App.vue -->
+<template>
+  <h1>
+    Our Vue app is running {{status}}!
+  </h1>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      status: 'well'
+    }
+  }
+}
+</script>
+<style>
+  h1 {
+    text-align: center;
+    font-size: 300%;
+  }
+</style>
+
+
